60e6d5424b3f0208e563a8b47a79c6b24347227c
Christina Poole <codenamedchris@gmail.com>
2020-12-18 16:41:50 -0500

    Updates

---
+++ precache-manifest.35f5e1a274d7c1bddc323e8689563e4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac8a829f75d046f61c360acf0f64653",
+    "url": "/laptop-customizaer/index.html"
+  },
+  {
+    "revision": "fb95496ae2cae41b9d3a",
+    "url": "/laptop-customizaer/static/css/main.df60c800.chunk.css"
+  },
+  {
+    "revision": "a3189dd90bd7726e1ec5",
+    "url": "/laptop-customizaer/static/js/2.6549ce60.chunk.js"
+  },
+  {
+    "revision": "fb95496ae2cae41b9d3a",
+    "url": "/laptop-customizaer/static/js/main.54c1b61b.chunk.js"
+  },
+  {
+    "revision": "676ff7f5ef54891d7104",
+    "url": "/laptop-customizaer/static/js/runtime~main.33d9b801.js"
+  }
+]);
